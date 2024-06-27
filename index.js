967e8077a24e8ddc8198466f42c2403977ce25be
allan <4lwachira@gmail.com>
2024-06-27 21:27:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const LeastFavoriteCustomer = 'parliament'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer ;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+var bestCustomer;
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "corruption";
+}
+
+
+   
+
